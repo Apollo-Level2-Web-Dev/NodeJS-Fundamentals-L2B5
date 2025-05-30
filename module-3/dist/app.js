99fb668192f0d4ac5c4a5011037b7b6a43b079eb
Samin Israr Ravi <saminisrar1@gmail.com>
2025-05-31 05:23:05 +0600

    done

---
+++ module-3/dist/app.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const todos_routes_1 = require("./app/todos/todos.routes");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const userRouter = express_1.default.Router();
+app.use("/todos", todos_routes_1.todosRouter);
+app.use("/users", userRouter);
+app.get('/', (req, res) => {
+    res.send('Welcome to Todos App');
+});
+// [app]-[express.json()]-[todosRouter]-[Root Route "/"]-[GET "/todos"]-[POST Create ToDo]
+//[todosRouter]-[get all todos /todos GET]-[create todo /todos/create-todo POST todo]
+exports.default = app;
+/**
+ * Basic File structure
+ * server - server handling like - starting, closing error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like create read update delete, database related works
+ */
---
+++ module-3/src/app.ts
@@ -0,0 +1,28 @@
+import express, { Application, Request, Response } from 'express';
+import { todosRouter } from './app/todos/todos.routes';
+const app: Application = express()
+
+app.use(express.json())
+
+const userRouter = express.Router()
+
+app.use("/todos", todosRouter)
+app.use("/users", userRouter)
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Todos App')
+})
+
+
+// [app]-[express.json()]-[todosRouter]-[Root Route "/"]-[GET "/todos"]-[POST Create ToDo]
+//[todosRouter]-[get all todos /todos GET]-[create todo /todos/create-todo POST todo]
+
+export default app;
+
+/**
+ * Basic File structure
+ * server - server handling like - starting, closing error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like create read update delete, database related works
+ */
